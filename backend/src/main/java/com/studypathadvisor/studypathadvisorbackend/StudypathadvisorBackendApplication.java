7548a0b8c8933a5e8c5c731d9a9c2247672c797a
lazar <lazar.ines28@gmail.com>
2024-02-29 10:51:52 +0100

    Initialisation projet

---
+++ backend/src/main/java/com/studypathadvisor/studypathadvisorbackend/StudypathadvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studypathadvisor.studypathadvisorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudypathadvisorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudypathadvisorBackendApplication.class, args);
+	}
+
+}
